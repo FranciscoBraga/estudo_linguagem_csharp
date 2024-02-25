bbd57b75e1220e11572f4f0a96d7e04539c77762
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-02-25 18:23:13 -0400

    Add files via upload

---
+++ C#/CalculadoraSimples/CalculadoraSimples/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.ReadKey();
+        Menu();
+    }
+
+    static void Menu(){
+        Console.WriteLine("Selecione uma opção");
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Divisão");
+        Console.WriteLine("4 - Multiplicação");
+        Console.WriteLine("5 - Sair");
+    
+
+    }
+}
