9e4aaba94acd28ecd5c6cf2ae163d33b14ffbd76
Francisco Oliveira Braga <associacaogames@gmail.com>
2024-03-28 10:06:49 -0400

    exemplos StopWatch e Editor de Texto
    Sao simples  programas para estudos em C#.   Parada de tempo utilizando treads e inicializando o editor de texto.

---
+++ C#/TextEditor/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    #nullable disable
+    private static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    static void Menu(){
+        Console.Clear();
+        Console.WriteLine("Seleciona um valor correspondente");
+        Console.WriteLine("0 - Sair");
+        Console.WriteLine("1 - Abrir Arquivo");
+        Console.WriteLine("2 - Criar novo Arquivo");
+
+        short option = short.Parse(Console.ReadLine());
+
+        switch(option){
+            case 0 :  System.Environment.Exit(0); break;
+            case 1 :  abrirArquivo(); break;
+            case 2 :  novoArquivo() ; break;
+            default : Menu(); break;
+        }
+
+        static void abrirArquivo(){
+
+        }
+
+         static void novoArquivo(){
+
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para Sair)");
+            Console.WriteLine("--------------------------------------");
+            string text = " ";
+
+            do{
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Write(text);
+
+            
+        }
+        
+    }
+}
